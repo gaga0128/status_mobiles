74480554b834149a00f799c78e3af4dadaac7946
AlbertPotter <smilepotter0128@gmail.com>
2016-04-22 23:03:56 +0700

    fix android-geth dependency

--- src/syng_im/components/chat/plain_message_input.cljs
+++ src/syng_im/components/chat/plain_message_input.cljs
@@ -3,7 +3,13 @@
             [syng-im.components.react :refer [android?
                                               view
                                               image
-                                              text-input]]
+                                              touchable-highlight
+                                              text-input
+                                              dismiss-keyboard]]
+            [syng-im.components.styles :refer [font
+                                               text2-color
+                                               color-white
+                                               color-blue]]
             [syng-im.components.chat.suggestions :refer [suggestions-view]]
             [syng-im.utils.utils :refer [log toast http-post]]
             [syng-im.utils.logging :as log]
@@ -13,51 +19,67 @@
 (defn set-input-message [message]
   (dispatch [:set-chat-input-text message]))
 
+(defn send [chat input-message]
+  (let [{:keys [group-chat chat-id]} chat]
+    (if group-chat
+      (dispatch [:send-group-chat-msg chat-id
+                 input-message])
+      (dispatch [:send-chat-msg chat-id
+                 input-message])))
+  (set-input-message nil)
+  (dismiss-keyboard))
+
 (defn plain-message-input-view []
   (let [chat (subscribe [:get-current-chat])
-        input-message-atom (subscribe [:get-chat-input-text])]
+        input-message-atom (subscribe [:get-chat-input-text])
+        staged-commands-atom (subscribe [:get-chat-staged-commands])]
     (fn []
       (let [input-message @input-message-atom]
         [view {:style {:flexDirection "column"}}
-        [suggestions-view]
-        [view {:style {:flexDirection   "row"
-                       :margin          1
-                       :height          40
-                       :backgroundColor "white"
-                       :borderRadius    5}}
-         [image {:source res/mic
-                 :style  {:marginTop  11
-                          :marginLeft 14
-                          :width      13
-                          :height     20}}]
-         [text-input {:underlineColorAndroid "transparent"
-                      :style                 {:flex       1
-                                              :marginLeft 18
-                                              :lineHeight 42
-                                              :fontSize   14
-                                              :fontFamily "Avenir-Roman"
-                                              :color      "#9CBFC0"}
-                      :autoFocus             true
-                      :placeholder           "Type your message here"
-                      :onChangeText          (fn [new-text]
-                                               (set-input-message new-text))
-                      :onSubmitEditing       (fn [e]
-                                               (let [{:keys [group-chat chat-id]} @chat]
-                                                 ;; TODO get text from state?
-                                                 (if group-chat
-                                                   (dispatch [:send-group-chat-msg chat-id
-                                                              input-message])
-                                                   (dispatch [:send-chat-msg chat-id
-                                                              input-message])))
-                                               (set-input-message nil))}
-          input-message]
-         [image {:source res/smile
-                 :style  {:marginTop   11
-                          :marginRight 12
-                          :width       18
-                          :height      18}}]
-         [image {:source res/att
-                 :style  {:marginTop   14
-                          :marginRight 16
-                          :width       17
-                          :height      14}}]]]))))
+         [suggestions-view]
+         [view {:style {:flexDirection     "row"
+                        :height            56
+                        :backgroundColor   color-white
+                        :elevation         4}}
+          [image {:source res/icon-list
+                  :style  {:marginTop    22
+                           :marginRight  6
+                           :marginBottom 6
+                           :marginLeft   21
+                           :width        13
+                           :height       12}}]
+          [text-input {:underlineColorAndroid "transparent"
+                       :style                 {:flex       1
+                                               :marginLeft 16
+                                               :marginTop  7
+                                               :fontSize   14
+                                               :fontFamily font
+                                               :color      text2-color}
+                       :autoFocus             false
+                       :placeholder           "Type"
+                       :placeholderTextColor  text2-color
+                       :onChangeText          (fn [new-text]
+                                                (set-input-message new-text))
+                       :onSubmitEditing       (fn [e]
+                                                (send @chat input-message))}
+           input-message]
+          [image {:source res/icon-smile
+                  :style  {:marginTop   18
+                           :marginRight 18
+                           :width       20
+                           :height      20}}]
+          (when (or (< 0 (count input-message))
+                    (< 0 (count @staged-commands-atom)))
+            [touchable-highlight {:on-press (fn []
+                                              (send @chat input-message))}
+             [view {:style {:marginTop       10
+                            :marginRight     10
+                            :width           36
+                            :height          36
+                            :borderRadius    50
+                            :backgroundColor color-blue}}
+              [image {:source res/icon-send
+                      :style  {:marginTop   10.5
+                               :marginLeft  12
+                               :width       15
+                               :height      15}}]]])]]))))
--- src/syng_im/persistence/realm.cljs
+++ src/syng_im/persistence/realm.cljs
@@ -1,6 +1,6 @@
 (ns syng-im.persistence.realm
   (:require [cljs.reader :refer [read-string]]
-            [syng-im.utils.debug :refer [log]]
+            [syng-im.utils.logging :as log]
             [syng-im.utils.types :refer [to-string]])
   (:refer-clojure :exclude [exists?]))
 
@@ -41,28 +41,12 @@
                      :primaryKey :chat-id
                      :properties {:chat-id    "string"
                                   :name       "string"
-                                  :group-chat "bool"
+                                  :group-chat {:type    "bool"
+                                               :indexed true}
+                                  :is-active  "bool"
                                   :timestamp  "int"
                                   :contacts   {:type       "list"
-                                               :objectType "chat-contact"}}}
-                    {:name        :tag
-                     :primaryKey  :name
-                     :properties  {:name         "string"
-                                   :count        {:type     "int"
-                                                  :optional true
-                                                  :default 1}}}
-                    {:name        :discoveries
-                     :primaryKey  :whisper-id
-                     :properties  {:name         "string"
-                                   :status       "string"
-                                   :whisper-id   "string"
-                                   :photo        "string"
-                                   :location     "string"
-                                   :tags         {:type       "list"
-                                                  :objectType "tag"}
-                                   :last-updated "date"}}
-
-                    ]})
+                                               :objectType "chat-contact"}}}]})
 
 
 (def realm (js/Realm. (clj->js opts)))
@@ -97,11 +81,6 @@
                                       value))]
     query))
 
-(defn get-by-filter [schema-name filter]
-  (let [_ (log filter)]
-  (-> (.objects realm (name schema-name))
-      (.filtered filter))))
-
 (defn get-by-field [schema-name field value]
   (let [q (to-query schema-name :eq field value)]
     (-> (.objects realm (name schema-name))
@@ -114,6 +93,9 @@
   (.sorted results (to-string field-name) (if (= order :asc)
                                             false
                                             true)))
+
+(defn filtered [results filter-query]
+  (.filtered results filter-query))
 
 (defn page [results from to]
   (js/Array.prototype.slice.call results from to))
@@ -134,8 +116,7 @@
   (read-string value))
 
 (defn delete [obj]
-  (write (fn []
-           (.delete realm obj))))
+  (.delete realm obj))
 
 (defn exists? [schema-name field value]
   (> (.-length (get-by-field schema-name field value))
@@ -145,27 +126,9 @@
   (.-length objs))
 
 (defn get-list [schema-name]
-  (vals (js->clj (.slice (.objects realm (to-string schema-name)) 0) :keywordize-keys true)))
+  (vals (js->clj (.objects realm (to-string schema-name)) :keywordize-keys true)))
 
 
 (comment
-
-  (write #(.create realm "msgs" (clj->js {:msg-id          "12"
-                                          :content         "sdfd"
-                                          :from            "sdfsd"
-                                          :chat-id         "56"
-                                          :content-type    "fg"
-                                          :timestamp       2
-                                          :outgoing        true
-                                          :to              "sfs"
-                                          :delivery-status "seen"}) true))
-
-  (.addListener realm "change" (fn [& args]
-                                 (log/debug args)))
-
-  ;realm.addListener('change', () => {
-  ;                                   // Update UI
-  ;                                   ...
-  ;                                   });
 
   )
