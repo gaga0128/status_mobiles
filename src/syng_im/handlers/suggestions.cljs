9c35a2cdf5aecfbb6fcc8d58674dc8c3be8b1e3e
AlbertPotter <smilepotter0128@gmail.com>
2016-04-25 12:05:54 +0300

    Return status view

--- src/syng_im/handlers/suggestions.cljs
+++ src/syng_im/handlers/suggestions.cljs
@@ -9,27 +9,23 @@
                                              get-chat-command-to-msg-id
                                              clear-staged-commands]]
             [syng-im.utils.utils :refer [log on-error http-get]]
-            [syng-im.utils.logging :as log]
-            [clojure.string :as s]))
-
-(defn suggestion? [text]
-  (= (get text 0) "!"))
+            [syng-im.utils.logging :as log]))
 
 (defn get-suggestions [db text]
-  (if (suggestion? text)
+  (if (= (get text 0) "!")
     ;; TODO change 'commands' to 'suggestions'
     (filterv #(.startsWith (:text %) text) (get-commands db))
     []))
 
 (defn get-command [db text]
-  (when (suggestion? text)
+  (when (= (get text 0) "!")
     ;; TODO change 'commands' to 'suggestions'
     (first (filter #(= (:text %) text) (get-commands db)))))
 
 (defn handle-command [db command-key content]
   (when-let [command-handler (get-chat-command-request db)]
-    (let [to-msg-id (get-chat-command-to-msg-id db)]
-      (command-handler to-msg-id command-key content)))
+   (let [to-msg-id (get-chat-command-to-msg-id db)]
+     (command-handler to-msg-id command-key content)))
   db)
 
 (defn get-command-handler [db command-key content]
@@ -57,11 +53,3 @@
 
 (defn load-commands []
   (http-get "chat-commands.js" execute-commands-js nil))
-
-(defn check-suggestion [db message]
-  (when-let [suggestion-text (when (string? message)
-                               (re-matches #"^![^\s]+\s" message))]
-    (let [suggestion-text' (s/trim suggestion-text)
-          [suggestion] (filter #(= suggestion-text' (:text %))
-                               (get-commands db))]
-      suggestion)))
