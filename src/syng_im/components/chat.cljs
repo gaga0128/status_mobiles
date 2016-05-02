4dc9709f4ee857f4a8431a100b70d63e26274620
AlbertPotter <smilepotter0128@gmail.com>
2016-05-02 16:56:42 +0300

    Start custom toolbar

--- src/syng_im/components/chat.cljs
+++ src/syng_im/components/chat.cljs
@@ -6,6 +6,7 @@
                                               text
                                               image
                                               navigator
+                                              touchable-highlight
                                               toolbar-android]]
             [syng-im.components.realm :refer [list-view]]
             [syng-im.components.styles :refer [font
@@ -14,12 +15,12 @@
                                                chat-background
                                                online-color
                                                selected-message-color
+                                               separator-color
                                                text1-color
                                                text2-color]]
             [syng-im.utils.logging :as log]
             [syng-im.navigation :refer [nav-pop]]
             [syng-im.resources :as res]
-            [syng-im.constants :refer [content-type-status]]
             [syng-im.utils.listview :refer [to-realm-datasource]]
             [syng-im.components.invertible-scroll-view :refer [invertible-scroll-view]]
             [reagent.core :as r]
@@ -42,7 +43,8 @@
                  :background-color background-color))))
 
 (defn chat-photo [{:keys [photo-path]}]
-  [view {:borderRadius 50}
+  [view {:margin       10
+         :borderRadius 50}
    [image {:source (if (s/blank? photo-path)
                      res/user-no-photo
                      {:uri photo-path})
@@ -53,8 +55,8 @@
 (defn contact-online [{:keys [online]}]
   (when online
     [view {:position        "absolute"
-           :top             20
-           :left            20
+           :top             30
+           :left            30
            :width           20
            :height          20
            :borderRadius    50
@@ -78,8 +80,7 @@
 
 (defn typing [member]
   [view {:style {:width         260
-                 :paddingTop    2
-                 :paddingBottom 8
+                 :marginTop     10
                  :paddingLeft   8
                  :paddingRight  8
                  :alignItems    "flex-start"
@@ -95,138 +96,147 @@
      (str member " is typing")]]])
 
 (defn typing-all []
-  [view {:style {:marginBottom 12}}
+  [view {:style {:marginBottom 20}}
    (for [member ["Geoff" "Justas"]]
      ^{:key member} [typing member])])
 
-(defn toolbar-content-chat [chat]
-  (let [group? (:group-chat chat)]
+(defn action-view [action]
+  [touchable-highlight {:on-press (:handler action)
+                        :underlay-color :transparent}
+   [view {:style {:flexDirection   "row"
+                  :height          56}}
+    [view {:width  56
+           :height 56}
+     [image {:source {:uri (:icon action)}
+             :style  (merge (:icon-style action)
+                            {:marginTop  21
+                             :marginLeft 23})}]]
     [view {:style {:flex 1
-                   :flexDirection "row"
-                   :backgroundColor "transparent"}}
-     [view {:style {:flex 1
-                    :alignItems "flex-start"
-                    :justifyContent "center"
-                    :marginRight 112}}
-      [text {:style {:marginTop  -2.5
-                     :color      text1-color
-                     :fontSize   16
-                     :fontFamily font}}
-       (or (chat :name)
-           "Chat name")]
-      (if group?
-        [view {:style {:flexDirection "row"}}
-         [image {:source {:uri "icon_group"}
-                 :style  {:marginTop 4
-                          :width     14
-                          :height    9}}]
-         [text {:style {:marginTop  -0.5
-                        :marginLeft 4
-                        :fontFamily font
+                   :alignItems "flex-start"
+                   :justifyContent "center"}}
+     [text {:style {:marginTop  -2.5
+                    :color      text1-color
+                    :fontSize   14
+                    :fontFamily font}}
+      (:title action)]
+     (when-let [subtitle (:subtitle action)]
+       [text {:style {:marginTop  1
+                      :color      text2-color
+                      :fontSize   12
+                      :fontFamily font}}
+        subtitle])]]])
+
+(defn actions-view [navigator chat]
+  (when-let [actions (when (or true (and (:group-chat chat) ;; temp
+                                         (:is-active chat)))
+                       [{:title      "Add Contact to chat"
+                         :icon       "icon_group"
+                         :icon-style {:width  14
+                                      :height 9}
+                         :handler    #(dispatch [:show-add-participants navigator])}
+                        {:title      "Remove Contact from chat"
+                         :subtitle   "Alex, John"
+                         :icon       "icon_search"
+                         :icon-style {:width  14
+                                      :height 9}
+                         :handler    #(dispatch [:show-remove-participants navigator])}
+                        {:title      "Leave Chat"
+                         :icon       "icon_search"
+                         :icon-style {:width  14
+                                      :height 9}
+                         :handler    #(dispatch [:leave-group-chat navigator])}])]
+    [view nil
+     [view {:style {:marginLeft 16
+                    :height 1.5
+                    :backgroundColor separator-color}}]
+     [view {:style {:marginVertical  10}}
+      (for [action actions]
+        ^{:key action} [action-view action])]]))
+
+(defn toolbar [navigator chat]
+  (let [show-actions true]
+    [view {:style {:flexDirection "column"
+                   :backgroundColor color-white
+                   :elevation       2}}
+     [view {:style {:flexDirection   "row"
+                    :height          56}}
+      (when (not show-actions)
+        [touchable-highlight {:on-press (fn []
+                                          (nav-pop navigator))
+                              :underlay-color :transparent}
+         [view {:width  56
+                :height 56}
+          [image {:source {:uri "icon_back"}
+                  :style  {:marginTop  21
+                           :marginLeft 23
+                           :width      8
+                           :height     14}}]]])
+      [view {:style {:flex 1
+                     :marginLeft (if show-actions 16 0)
+                     :alignItems "flex-start"
+                     :justifyContent "center"}}
+       [text {:style {:marginTop  -2.5
+                      :color      text1-color
+                      :fontSize   16
+                      :fontFamily font}}
+        (or (chat :name)
+            "Chat name")]
+       (if (:group-chat chat)
+         [view {:style {:flexDirection "row"}}
+          [image {:source {:uri "icon_group"}
+                  :style  {:marginTop 4
+                           :width     14
+                           :height    9}}]
+          [text {:style {:marginTop  -0.5
+                         :marginLeft 4
+                         :fontFamily font
+                         :fontSize   12
+                         :color      text2-color}}
+           (str (count (:contacts chat))
+                (if (< 1 (count (:contacts chat)))
+                  " members"
+                  " member")
+                ", " (count (:contacts chat)) " active")]]
+         [text {:style {:marginTop  1
+                        :color      text2-color
                         :fontSize   12
-                        :color      text2-color}}
-          (str (count (:contacts chat))
-               (if (< 1 (count (:contacts chat)))
-                 " members"
-                 " member")
-               ", " (count (:contacts chat)) " active")]]
-        [text {:style {:marginTop  1
-                       :color      text2-color
-                       :fontSize   12
-                       :fontFamily font}}
-         "Active a minute ago"])]
-     (when-not group?
-       [view {:style {:position "absolute"
-                      :top      10
-                      :right    66}}
-        [chat-photo {}]
-        [contact-online {:online true}]])]))
+                        :fontFamily font}}
+          "Active a minute ago"])]
+      (when-not (:group-chat chat)
+        [touchable-highlight {:on-press (fn []
+                                          (nav-pop navigator))
+                              :underlay-color :transparent}
+         [view {:style {:width  56
+                        :height 56}}
+          [chat-photo {}]
+          [contact-online {:online true}]]])]
+     [actions-view navigator chat]]))
 
 (defn chat [{:keys [navigator]}]
-  (let [messages (subscribe [:get-chat-messages])
-        chat     (subscribe [:get-current-chat])]
+  (let [messages          (subscribe [:get-chat-messages])
+        chat              (subscribe [:get-current-chat])]
     (fn []
       (let [msgs                @messages
                                         ;_                 (log/debug "messages=" msgs)
-            ;; temp to show first status
-            msgs-clj (as-> (js->clj msgs) ms
-                       (assoc ms "-1"
-                              {:msg-id "-1"
-                               :content (str "The brash businessman’s braggadocio "
-                                             "and public exchange with candidates "
-                                             "in the US presidential election")
-                               :delivery-status "seen"
-                               :from "Status"
-                               :chat-id "-"
-                               :content-type content-type-status
-                               :timestamp 1
-                               "outgoing" false
-                               :to nil})
-                       (reduce (fn [items [n m]]
-                                 (assoc items (.parseInt js/window n) m
-                                        ;; (assoc m "from" (if (< 0.5 (rand))
-                                        ;;                   "Status"
-                                        ;;                   "abc"))
-                                        ))
-                               {}  ms)
-                       (into (sorted-map) ms)
-                       (map (fn [[n current] [_ next]]
-                              [n (-> current
-                                     (assoc :same-author
-                                            (if next
-                                              (= (get current "from") (get next "from"))
-                                              true))
-                                     (assoc :same-direction
-                                            (if next
-                                              (= (get current "outgoing") (get next "outgoing"))
-                                              true))
-                                     (assoc :last-msg (= 0 n)))
-                               current])
-                            ms (conj (vec (rest ms)) nil))
-                       (reduce (fn [items [n m]]
-                                 (assoc items n m))
-                               {}  ms))
-            msgs (clj->js msgs-clj)
+            ;; temp
+            typing (:group-chat @chat)
             ;; end temp
             datasource          (to-realm-datasource msgs)
             contacts            (:contacts @chat)
             contact-by-identity (contacts-by-identity contacts)]
         [view {:style {:flex            1
                        :backgroundColor chat-background}}
-         (when android?
-           ;; TODO add IOS version
-           [toolbar-android {:navIcon       {:uri "icon_back"}
-                             :style         {:backgroundColor color-white
-                                             :height          56
-                                             :elevation       2}
-                             :actions          (when (and (:group-chat @chat)
-                                                          (:is-active @chat))
-                                                 [{:title        "Add Contact to chat"
-                                                   :icon         res/add-icon
-                                                   :showWithText true}
-                                                  {:title        "Remove Contact from chat"
-                                                   :icon         res/trash-icon
-                                                   :showWithText true}
-                                                  {:title        "Leave Chat"
-                                                   :icon         res/leave-icon
-                                                   :showWithText true}])
-                             :onActionSelected (fn [position]
-                                                 (case position
-                                                   0 (dispatch [:show-add-participants navigator])
-                                                   1 (dispatch [:show-remove-participants navigator])
-                                                   2 (dispatch [:leave-group-chat navigator])))
-                             :onIconClicked    (fn []
-                                                 (nav-pop navigator))}
-            [toolbar-content-chat @chat]])
-         [list-view {:dataSource            datasource
-                     :renderScrollComponent (fn [props]
-                                              (invertible-scroll-view (js->clj props)))
-                     :renderRow             (fn [row section-id row-id]
-                                              (let [msg (-> (js->clj row :keywordize-keys true)
-                                                            (add-msg-color contact-by-identity)
-                                                            (assoc :group-chat (:group-chat @chat)))]
-                                                (r/as-element [chat-message msg])))
-                     :style                 {:backgroundColor "white"}}]
+         [toolbar navigator @chat]
+         (let [last-msg-id (:last-msg-id @chat)]
+           [list-view {:dataSource            datasource
+                       :renderScrollComponent (fn [props]
+                                                (invertible-scroll-view (js->clj props)))
+                       :renderRow             (fn [row section-id row-id]
+                                                (let [msg (-> (js->clj row :keywordize-keys true)
+                                                              (add-msg-color contact-by-identity)
+                                                              (assoc :group-chat (:group-chat @chat))
+                                                              (assoc :typing typing))]
+                                                  (r/as-element [chat-message msg last-msg-id])))}])
          (when (:group-chat @chat)
            [typing-all])
          (when (:is-active @chat)
