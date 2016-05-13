b813b0d9bd115d3668b3ed70f8d64cfb79e7f4eb
AlbertPotter <smilepotter0128@gmail.com>
2016-05-13 15:15:40 +0300

    Group settings refactoring

--- src/syng_im/models/protocol.cljs
+++ src/syng_im/models/protocol.cljs
@@ -6,19 +6,25 @@
             [syng-im.utils.types :refer [to-edn-string]]
             [re-frame.db :refer [app-db]]
             [syng-im.db :as db]
-            [syng-im.persistence.simple-kv-store :as kv]))
+            [syng-im.persistence.simple-kv-store :as kv]
+            [syng-im.utils.logging :as log]))
 
 (defn set-initialized [db initialized?]
   (assoc-in db db/protocol-initialized-path initialized?))
 
 (defn update-identity [db identity]
-  (let [password  (:identity-password db)
+  (let [password  (get-in db db/identity-password-path)
         encrypted (password-encrypt password (to-edn-string identity))]
     (s/put kv/kv-store :identity encrypted)
     (assoc db :user-identity identity)))
 
 (defn stored-identity [db]
   (let [encrypted (s/get kv/kv-store :identity)
-        password  (:identity-password db)]
+        password  (get-in db db/identity-password-path)]
     (when encrypted
       (read-string (password-decrypt password encrypted)))))
+
+(comment
+
+  (stored-identity @re-frame.db/app-db)
+  )
