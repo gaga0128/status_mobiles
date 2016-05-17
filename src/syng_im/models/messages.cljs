6c86ed4f08721b051822b0cd0c62e9d937b907b5
AlbertPotter <smilepotter0128@gmail.com>
2016-05-17 13:32:20 +0300

    more cleanup in handlers/subs

--- src/syng_im/models/messages.cljs
+++ src/syng_im/models/messages.cljs
@@ -1,6 +1,5 @@
 (ns syng-im.models.messages
   (:require [syng-im.persistence.realm :as r]
-            [re-frame.core :refer [dispatch]]
             [cljs.reader :refer [read-string]]
             [syng-im.utils.random :refer [timestamp]]
             [syng-im.db :as db]
@@ -18,29 +17,28 @@
   (keywordize-keys (apply hash-map (split s #"[;=]"))))
 
 (defn save-message
-  [chat-id {:keys [from to msg-id content content-type outgoing
-                   same-author same-direction]
+  ;; todo remove chat-id parameter
+  [chat-id {:keys [to msg-id content outgoing]
+            ;; outgoing should be explicitely defined in handlers
             :or {outgoing false
-                 to       nil} :as msg}]
-  (log/debug "save-message" chat-id msg)
+                 to       nil} :as message}]
   (when-not (r/exists? :msgs :msg-id msg-id)
     (r/write
       (fn []
-        (let [content      (if (string? content)
-                             content
-                             (map-to-str content))]
-          (r/create :msgs {:chat-id         chat-id
-                           :msg-id          msg-id
-                           :from            from
-                           :to              to
-                           :content         content
-                           :content-type    content-type
-                           :outgoing        outgoing
-                           :timestamp       (timestamp)
-                           :delivery-status nil
-                           ;; TODO 'some?' is temp
-                           :same-author     (some? same-author)
-                           :same-direction  (some? same-direction)} true))))))
+        (let [content' (if (string? content)
+                         content
+                         (map-to-str content))
+              message' (merge message
+                              {:chat-id         chat-id
+                               :content         content'
+                               :timestamp       (timestamp)
+                               :delivery-status nil})]
+          (r/create :msgs message' true))))))
+
+(defn command-type? [type]
+  (contains?
+    #{c/content-type-command c/content-type-command-request}
+    type))
 
 (defn get-messages [chat-id]
   (->> (-> (r/get-by-field :msgs :chat-id chat-id)
@@ -48,8 +46,7 @@
            (r/collection->map))
        (into '())
        (map (fn [{:keys [content-type] :as message}]
-              (if (#{c/content-type-command c/content-type-command-request}
-                    content-type)
+              (if (command-type? content-type)
                 (update message :content str-to-map)
                 message)))))
 
@@ -59,10 +56,3 @@
     (fn []
       (when (r/exists? :msgs :msg-id msg-id)
         (r/create :msgs msg true)))))
-
-(defn clear-history [chat-id]
-  (r/write
-   (fn []
-     (r/delete (r/get-by-field :msgs :chat-id chat-id))))
-  ;; TODO temp. Update chat in db atom
-  (dispatch [:initialize-chats]))
