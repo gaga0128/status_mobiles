4f4c4cb7a793aa493a87e4846185f6b009446180
AlbertPotter <smilepotter0128@gmail.com>
2016-06-16 13:11:01 +0300

    Move drawer to tabs view

--- src/status_im/contacts/styles.cljs
+++ src/status_im/contacts/styles.cljs
@@ -2,10 +2,11 @@
   (:require [status-im.components.styles :refer [font
                                                  title-font
                                                  text1-color
-                                                 text3-color
                                                  color-white
                                                  toolbar-background2
                                                  online-color]]))
+
+
 
 (def contacts-list-container
   {:flex            1
@@ -14,30 +15,55 @@
 (def contacts-list
   {:backgroundColor :white})
 
-(def letter-container
-  {:paddingTop  11
-   :paddingLeft 20
-   :width       56})
-
-(def letter-text
-  {:fontSize   24
-   :fontFamily font
-   :color      text3-color})
-
 (def contact-photo-container
-  {:marginTop  4
-   :marginLeft 12})
+  {:borderRadius 50})
+
+(def photo-image
+  {:borderRadius 50
+   :width        40
+   :height       40})
+
+(def online-container
+  {:position        :absolute
+   :top             24
+   :left            24
+   :width           20
+   :height          20
+   :borderRadius    50
+   :backgroundColor online-color
+   :borderWidth     2
+   :borderColor     color-white})
+
+(def online-dot
+  {:position        :absolute
+   :top             6
+   :width           4
+   :height          4
+   :borderRadius    50
+   :backgroundColor color-white})
+
+(def online-dot-left
+  (assoc online-dot :left 3))
+
+(def online-dot-right
+  (assoc online-dot :left 9))
 
 (def contact-container
   {:flexDirection :row
    :height        56})
 
+(def photo-container
+  {:marginTop  8
+   :marginLeft 16
+   :width      44
+   :height     44})
+
 (def name-container
-  {:marginLeft     12
-   :justifyContent :center})
+  {:justifyContent :center})
 
 (def name-text
-  {:fontSize   16
+  {:marginLeft 16
+   :fontSize   16
    :fontFamily font
    :color      text1-color})
 
