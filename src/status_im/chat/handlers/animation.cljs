6c8aa177dd18795d737499d7c55eaa08b041986d
AlbertPotter <smilepotter0128@gmail.com>
2016-06-27 12:07:14 +0300

    custom register-handler & response icon animation

--- src/status_im/chat/handlers/animation.cljs
+++ src/status_im/chat/handlers/animation.cljs
@@ -1,8 +1,10 @@
 (ns status-im.chat.handlers.animation
-  (:require [re-frame.core :refer [register-handler after dispatch debug]]
+  (:require [re-frame.core :refer [after dispatch debug]]
+            [status-im.utils.handlers :refer [register-handler]]
             [re-frame.middleware :refer [path]]
             [status-im.handlers.content-suggestions :refer [get-content-suggestions]]
             [status-im.chat.constants :refer [input-height request-info-height
+                                              minimum-command-suggestions-height
                                               response-height-normal minimum-suggestion-height]]
             [status-im.constants :refer [response-input-hiding-duration]]))
 
@@ -19,58 +21,71 @@
   (fn [db _]
     (assoc db
       :to-response-height input-height
-      :messages-offset 0)))
+      :messages-offset? false)))
 
-(defn get-response-height
-  [{:keys [current-chat-id] :as db}]
-  (let [suggestions        (get-in db [:suggestions current-chat-id])
-        suggestions-height (if suggestions middle-height 0)]
-    (+ input-height
-       (min response-height-normal (+ suggestions-height request-info-height)))))
+(def response-height (+ input-height response-height-normal))
 
 (defn update-response-height [db]
-  (assoc-in db [:animations :to-response-height] (get-response-height db)))
+  (assoc-in db [:animations :to-response-height] response-height))
+
+(register-handler :animate-command-suggestions
+  (fn [{:keys [current-chat-id] :as db} _]
+    (let [suggestions? (seq (get-in db [:command-suggestions current-chat-id]))
+          current (get-in db [:animations :command-suggestions-height])
+          height (if suggestions? middle-height 0.1)]
+      (-> db
+          (update :animations assoc
+                  :messages-offset? suggestions?
+                  :messages-offset-max 22
+                  :command-suggestions-height height)
+          (update-in [:animations :commands-height-changed]
+                     (if (and suggestions? (not= 0.1 current))
+                       identity inc))))))
 
 (register-handler :animate-show-response
   [(after #(dispatch [:command-edit-mode]))]
-  (fn [db _]
-    (-> db
-        (assoc-in [:animations :messages-offset] request-info-height)
-        (update-response-height))))
+  (fn [{:keys [current-chat-id] :as db}]
+    (let [suggestions? (seq (get-in db [:suggestions current-chat-id]))
+          height (if suggestions? middle-height minimum-suggestion-height)]
+      (update db :animations assoc :messages-offset? true
+              :messages-offset-max request-info-height
+              :to-response-height height))))
 
-(animation-handler :set-response-max-height
-  (fn [db [_ height]]
-    (let [prev-height (:response-height-max db)]
-      (if (not= height prev-height)
-        (let [db (assoc db :response-height-max height)]
-          (if (= prev-height (:to-response-height db))
-            (assoc db :to-response-height height)
-            db))
-        db))))
+(defn fix-height
+  [height-key height-signal-key suggestions-key minimum]
+  (fn [{:keys [current-chat-id] :as db} [_ vy current]]
+    (let [max-height (get-in db [:layout-height])
+          moving-down? (pos? vy)
+          moving-up? (not moving-down?)
+          under-middle-position? (<= current middle-height)
+          over-middle-position? (not under-middle-position?)
+          suggestions (get-in db [suggestions-key current-chat-id])
+          new-fixed (cond (not suggestions)
+                          minimum
+
+                          (and under-middle-position? moving-up?)
+                          middle-height
+
+                          (and over-middle-position? moving-down?)
+                          middle-height
+
+                          (and over-middle-position? moving-up?)
+                          max-height
+
+                          (and under-middle-position? moving-down?)
+                          minimum)]
+      (-> db
+          (assoc-in [:animations height-key] new-fixed)
+          (update-in [:animations height-signal-key] inc)))))
+
+(register-handler :fix-commands-suggestions-height
+  (fix-height :command-suggestions-height
+              :commands-height-changed
+              :command-suggestions
+              minimum-command-suggestions-height))
 
 (register-handler :fix-response-height
-  (fn [{:keys [current-chat-id] :as db} [_ vy current]]
-    (let [max-height             (get-in db [:animations :response-height-max])
-          moving-down?           (pos? vy)
-          moving-up?             (not moving-down?)
-          under-middle-position? (<= current middle-height)
-          over-middle-position?  (not under-middle-position?)
-          suggestions            (get-in db [:suggestions current-chat-id])
-          new-fixed              (cond (not suggestions)
-                                       minimum-suggestion-height
-
-                                       (and under-middle-position? moving-up?)
-                                       middle-height
-
-                                       (and over-middle-position? moving-down?)
-                                       middle-height
-
-                                       (and over-middle-position? moving-up?)
-                                       max-height
-
-                                       (and under-middle-position?
-                                            moving-down?)
-                                       minimum-suggestion-height)]
-      (-> db
-          (assoc-in [:animations :to-response-height] new-fixed)
-          (update-in [:animations :response-height-changed] inc)))))
+  (fix-height :to-response-height
+              :response-height-changed
+              :suggestions
+              minimum-suggestion-height))
