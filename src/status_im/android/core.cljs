0579df8206330bfd3e985098ce3c853ca4fe5e03
AlbertPotter <smilepotter0128@gmail.com>
2016-07-05 18:56:17 +0300

    update select user screen

--- src/status_im/android/core.cljs
+++ src/status_im/android/core.cljs
@@ -14,6 +14,8 @@
             [status-im.discovery.screen :refer [discovery]]
             [status-im.discovery.tag :refer [discovery-tag]]
             [status-im.chat.screen :refer [chat]]
+            [status-im.login.screen :refer [login]]
+            [status-im.users.screen :refer [users]]
             [status-im.chats-list.screen :refer [chats-list]]
             [status-im.new-group.screen :refer [new-group]]
             [status-im.participants.views.add :refer [new-participants]]
@@ -76,6 +78,8 @@
            :qr-scanner [qr-scanner]
            :chat [chat]
            :profile [profile]
+           :users [users]
+           :login [login]
            :my-profile [my-profile]))})))
 
 (defn init []
