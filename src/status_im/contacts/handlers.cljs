5234e1ab71a7196b1e10587d249d61f7cc28544a
AlbertPotter <smilepotter0128@gmail.com>
2016-11-08 12:08:51 +0300

    Updated animation (#418)

--- src/status_im/contacts/handlers.cljs
+++ src/status_im/contacts/handlers.cljs
@@ -172,8 +172,7 @@
   (u/side-effect!
     (fn [_ [_ {:keys [whisper-identity] :as contact}]]
       (when-not (contacts/get-by-id whisper-identity)
-        (dispatch [::prepare-contact contact])
-        (dispatch [:start-chat whisper-identity {} :navigation-replace])))))
+        (dispatch [::prepare-contact contact])))))
 
 (register-handler ::prepare-contact
   (-> add-new-contact
